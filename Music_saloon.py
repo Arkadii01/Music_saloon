5a8ade1ce5494f6dd397c1dfa1a893a73546e808
Arkadii01 <arkadij.t@bk.ru>
2025-02-06 11:31:55 +0700

    Добавление окна с регистрацией

--- Music_saloon.py
+++ Music_saloon.py
@@ -1,1 +1,16 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
+class RegistrationWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('windows/enter_window.ui', self)
+        self.registration_part.hide()
+        
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    registration = RegistrationWindow()
+    registration.show()
+    sys.exit(app.exec())
